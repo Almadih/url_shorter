3815b8e13a888d035ceac48d2e4b22682a8341f3
Almadih <modialmadih@gmail.com>
2023-01-03 13:56:11 +0200

    update static html

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const URL_SHORTENER_API_ENDPOINT = "https://blue-cell-d0fc.almadih.workers.dev/";
+export const URL_SHORTENER_API_ENDPOINT = "https://url-shorter.almadih.workers.dev/";
 export const BASE_URL = window.location.href
